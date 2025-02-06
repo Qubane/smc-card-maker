44686b31cfcc3a8fd8f600cb2e6f0fc808877529
UltraQbik <no1skill@yandex.ru>
2025-02-07 00:35:17 +0300

    Add CardMaker namespace class

--- source/makers.py
+++ source/makers.py
@@ -1,3 +1,19 @@
 """
 Card makers
 """
+
+
+from typing import Any
+
+
+class CardMaker:
+    """
+    Base card maker namespace
+    """
+
+    @staticmethod
+    def make_card(data: Any) -> Any:
+        """
+        Makes a card
+        """
+
