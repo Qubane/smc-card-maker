fd81e6a9a8d21096fbcc6039495f7bd9dbe72c5f
UltraQbik <no1skill@yandex.ru>
2025-02-06 23:22:45 +0300

    Add Application class

--- source/application.py
+++ source/application.py
@@ -1,3 +1,17 @@
 """
 Application class
 """
+
+
+class Application:
+    """
+    Application class
+    """
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        """
+        Runs the application
+        """
