b798831736de5d1b9c1d49b53395ccce0ed3ca62
UltraQbik <no1skill@yandex.ru>
2025-02-06 23:50:38 +0300

    Add all colors class

--- source/classes.py
+++ source/classes.py
@@ -1,3 +1,59 @@
 """
 Useful classes
 """
+
+
+class Colors:
+    """
+    Class containing all colors available in a paint tool
+    """
+
+    GRAY_0 = "222222"
+    GRAY_1 = "4A4A4A"
+    GRAY_2 = "7F7F7F"
+    GRAY_3 = "EEEEEE"
+
+    YELLOW_0 = "323000"
+    YELLOW_1 = "817C00"
+    YELLOW_2 = "E2DB13"
+    YELLOW_3 = "F5F071"
+
+    LIME_0 = "375000"
+    LIME_1 = "577D07"
+    LIME_2 = "A0EA00"
+    LIME_3 = "CBF66F"
+
+    GREEN_0 = "064023"
+    GREEN_1 = "0E8031"
+    GREEN_2 = "19E753"
+    GREEN_3 = "68FF88"
+
+    CYAN_0 = "0A4444"
+    CYAN_1 = "118787"
+    CYAN_2 = "2CE6E6"
+    CYAN_3 = "7EEDED"
+
+    BLUE_0 = "0A1D5A"
+    BLUE_1 = "0F2E91"
+    BLUE_2 = "0A3EE2"
+    BLUE_3 = "4C6FE3"
+
+    VIOLET_0 = "35086C"
+    VIOLET_1 = "500AA6"
+    VIOLET_2 = "7514ED"
+    VIOLET_3 = "AE79F0"
+
+    MAGENTA_0 = "520653"
+    MAGENTA_1 = "720A74"
+    MAGENTA_2 = "CF11D2"
+    MAGENTA_3 = "EE7BF0"
+
+    RED_0 = "560202"
+    RED_1 = "7C0000"
+    RED_2 = "D02525"
+    RED_3 = "F06767"
+
+    ORANGE_0 = "472800"
+    ORANGE_1 = "673B00"
+    ORANGE_2 = "DF7F00"
+    ORANGE_3 = "EEAF5C"
