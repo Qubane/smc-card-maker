ffcde0216905d93be797e3ded48e42cc155c3631
UltraQbik <no1skill@yandex.ru>
2025-02-07 22:39:50 +0300

    Make basic file decoding

--- source/makers.py
+++ source/makers.py
@@ -3,7 +3,7 @@ Card makers
 """
 
 
-from typing import Any
+from io import BytesIO
 
 
 class CardMaker:
@@ -28,6 +28,27 @@ class QCPUCardMaker(CardMaker):
         """
         Makes a card for QCPU's
         """
+
+        # make byte stream
+        stream = BytesIO(data)
+
+        # read namespace
+        namespace: bytes = b''
+        while (char := stream.read(1)) != b'\x00':
+            namespace += char
+
+        # pick offset
+        if namespace == b'QT':
+            offset = 4
+        elif namespace == b'QM':
+            offset = 3
+        else:
+            raise NotImplementedError
+
+        # read instructions
+        instructions: list[bytes] = []
+        while word := stream.read(offset):
+            instructions.append(word)
 
 
 class MCPU20CardMaker(CardMaker):
